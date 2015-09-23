6dc33cefb2c8d507e76eb07f7d2c39cfe796ef99
Sandro Bonazzola <sbonazzo@redhat.com>
2015-09-23 17:46:47 +0200

    automation: add standard build automation
    Change-Id: I8bd20509100fe9764cb951699b2273bcb0018c59
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

---
+++ automation/check-patch.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -xe
+[[ -d exported-artifacts ]] \
+|| mkdir -p exported-artifacts
+
+[[ -d rpmbuild ]] \
+|| mkdir -p rpmbuild
+
+make clean all
+make dist
+rpmbuild \
+    --define "_topmdir $PWD/rpmbuild" \
+    --define "_rpmdir $PWD/rpmbuild" \
+    -ta *.tar.gz
+
+# Move RPMs to exported artifacts
+find rpmbuild -iname \*rpm -exec mv "{}" exported-artifacts/ \;
+cp -l *.tar.gz exported-artifacts/
